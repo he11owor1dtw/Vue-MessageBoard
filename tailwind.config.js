703464990075a0f7970e4ca7eda1fae259c61351
HelloCodeman <955khm@gmail.com>
2024-06-04 20:56:03 +0800

    利用 Vite + Tailwind CSS 建立開發環境

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html", // Tailwind CSS 會掃描 index.html 文件並自動移除未使用的樣式
+    "./src/**/*.{js,jsx,ts,tsx,vue}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+

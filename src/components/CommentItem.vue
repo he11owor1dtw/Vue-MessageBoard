c4d1da40e50f6eff76515669bd723488ad2bfed4
HelloCodeman <955khm@gmail.com>
2024-06-06 17:22:37 +0800

    定義 props 屬性讓組件接收並將原先寫死的區塊更改成屬性值

--- src/components/CommentItem.vue
+++ src/components/CommentItem.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="flex">
     <!-- 即使在 public 文件夾下，也需要使用 / 絕對路徑引用它下面的文件，省略 Public -->
-    <img src="/images/face1.png" alt="" class="w-12 h-12 mr-4 rounded-full" /> <!-- rounded-full 方形變圓形 -->
+    <img :src="avatar" alt="" class="w-12 h-12 mr-4 rounded-full" /> <!-- rounded-full 方形變圓形 -->
     <div>
-      <p>機器人 1 號</p>
-      <p class="text-gray-600 text-sm">18 小時之前</p>
+      <p>{{ user }}</p>
+      <p class="text-gray-600 text-sm">{{ time }}</p>
     </div>
     <!-- 三點圖標：flex 佈局下 ml-auto 向右對齊 -->
     <span class="ml-auto">
@@ -14,11 +14,13 @@
     </span>
   </div>
   <p class="text-gray-600 py-4">
-    哇！這篇文章真是寫的太好啦！收到很大的啟發，希望你再接再厲，發表越來越多文章！
+    {{ content }}
   </p>
 </template>
 
 <script setup>
+defineProps(["user", "avatar", "time", "content"]);
+// 接收名字、頭像、發布時間、留言内容
 </script>
 
 <style></style>

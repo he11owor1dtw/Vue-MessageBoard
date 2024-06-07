6f8e8114eaaefdcac530a63930e5506d590b0dc0
HelloCodeman <955khm@gmail.com>
2024-06-07 09:35:55 +0800

    傳遞數據到父组件

--- src/components/CommentBox.vue
+++ src/components/CommentBox.vue
@@ -1,7 +1,8 @@
 <template>
   <!-- 留言區 -->
-  <form action="" class="grid">
-    <textarea name="comment" id="" placeholder="請輸入你的評論" class="bg-gray-50 p-2 rounded-xl"></textarea>
+  <form action="" class="grid" @submit="handleSubmit">
+    <!-- 用戶在輸入内容時，使用 content 捕捉輸入的內容 -->
+    <textarea v-model="content" name="comment" id="" placeholder="請輸入你的評論" class="bg-gray-50 p-2 rounded-xl"></textarea>
     <fieldset class="py-4">
       <input type="submit" value="留言" class="px-4 py-1 bg-blue-600 rounded text-white">
       <input type="reset" value="取消" class="px-4 py-1 bg-yellow-400 rounded border ml-3">
@@ -10,9 +11,16 @@
 </template>
 
 <script>
-export default {
+import { ref } from "vue";
 
-}
+const content = ref("");
+const emit = defineEmits(["submit"]);
+const handleSubmit = (e) => {
+  e.preventDefault();
+  emit("submit", content.value);
+  content.value = "";
+};
+
 </script>
 
 <style></style>

731a38a6d36a029dc0183ad0a44c6f42bd659940
HelloCodeman <955khm@gmail.com>
2024-06-11 16:18:36 +0800

    重新設定 proxy 並讀取留言列表

--- src/CommentApp.vue
+++ src/CommentApp.vue
@@ -2,28 +2,19 @@
   <main class="p-4 bg-gray-50 min-h-screen">
     <div class="max-w-screen-xl mx-auto bg-white p-8 rounded-lg shadow-2xl">
       <h2 class="text-3xl my-6">說說你的想法吧</h2>
-      <CommentBox @submit="addNewComment"/>
+      <CommentBox @submit="addNewComment" />
       <!-- 分隔線 -->
       <DividerHorizontal />
       <!-- 留言區 -->
       <div v-for="comment in comments" :key="comment.id">
-        <CommentItem 
-        :user="comment.user" 
-        :avatar="comment.avatar" 
-        :time="comment.time" 
-        :content="comment.content" />
+        <CommentItem :user="comment.user" :avatar="comment.avatar" :time="comment.time" :content="comment.content" />
         <!-- 回覆區 -->
         <ReplyContainer v-if="comment.replies">
           <!-- 回覆內容 -->
-          <CommentItem 
-          v-for="reply in comment.replies"
-          :key="reply.id"
-          :user="reply.user"
-          :avatar="reply.avatar" 
-          :time="reply.time" 
-          :content="reply.content" />
+          <CommentItem v-for="reply in comment.replies" :key="reply.id" :user="reply.user" :avatar="reply.avatar"
+            :time="reply.time" :content="reply.content" />
         </ReplyContainer>
-        <ReplyBox @submit="addReply($event, comment.id)"/>
+        <ReplyBox @submit="addReply($event, comment.id)" />
         <!-- 
         當 submit 事件觸發時，調用 addReply 方法，傳入回覆內容（$event）和當前評論的 ID（comment.id）
         $event 事件數據，即 CommentBox 傳遞過來的用戶輸入的回覆內容，
@@ -36,7 +27,7 @@
 
 <!-- 有 setup 就不用 export，反之需要 export 給組件使用 -->
 <script setup>
-import { ref } from "vue";
+import { ref, onMounted } from "vue";
 
 import CommentBox from "./components/CommentBox.vue";
 import CommentItem from "./components/CommentItem.vue";
@@ -52,33 +43,21 @@ import face4 from "./assets/face4.png";
 let rid = ref(4);
 
 // 先定義為常量，之後會定義為 ref
-const comments = ref([
-  {
-    id: 1,
-    user: "Jenny",
-    avatar: face1,
-    time: "5小時之前",
-    content:
-      "哇！這篇真是寫的太好了，收到很大的啟發，希望能夠繼續產出更多優質內容！",
-    replies: [
-      {
-        id: 2,
-        user: "Henry",
-        avatar: face2,
-        time: "4小時之前",
-        content: "認同！",
-      },
-      {
-        id: 3,
-        user: "Oscar",
-        avatar: face3,
-        time: "1小時之前",
-        content:
-          "這篇寫的非常好，兼具專業與實務方面，也有自己的看法，真的是非常好的文章。",
-      },
-    ],
-  },
-]);
+const comments = ref([]);
+
+async function getAllComments() {
+  const res = await fetch("/api/comments");
+  comments.value = await res.json();
+}
+
+onMounted(() => {
+  getAllComments();
+});
+
+const addNewComment = (content) => {
+  const newComment = constructNewComment(content);
+  comments.value.push(newComment);
+};
 
 const constructNewComment = (content) => {
   return {
@@ -90,17 +69,12 @@ const constructNewComment = (content) => {
   };
 };
 
-const addNewComment = (content) => {
-  const newComment = constructNewComment(content);
-  comments.value.push(newComment);
-};
-
 const addReply = (content, id) => {
   // 接收兩個參數：content 和 id，content 是回覆的內容，id 是評論的唯一標識符，用來確定要回覆哪一條評論
   const reply = constructNewComment(content);
   // constructNewComment 是一個函數，用來生成一個新的回覆對象。
   // 該函數接收 content 作為參數，並返回一個包含回覆內容及其他訊息（如時間、用戶、ID 等）的對象。  
-  
+
   let comment = comments.value.find((comment) => comment.id === id);
   // 根據傳入的 id，找到對應的評論對象，準備將新的回覆添加到這個評論的回覆列表中。
 
--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,19 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:3001",
+        // 設置對象為 "/api" ，對於所有 /api 的請求都交由這個代理去處理
+        changeOrigin: true,
+        // 設置 changeOrigin 為 true，發送請求的時候修改請求的 origin，避免跨域
+        rewrite: (path) => path.replace(/^\/api/, ""),
+        // 設置 rewrite，把實際請求的 /api 這段去掉，後端的服務 URL 中，沒有 /api 這一段，
+        // 只是方便前端區分是否爲調用後端的 URL
+      },
+    },
+  },
 })
 
 // 這段 Vite 配置文件的用意是設定 Vite 專案的配置，並啟用 Vue 插件來支持 Vue.js 開發
